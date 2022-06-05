25396c0017682b085e04a48320df7d9518f044c5
bSatierf <balbino.fn@gmail.com>
2022-06-05 02:30:50 -0300

    add mensagem espaço vazio, add informação sobre palavra repetida

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -7,16 +7,18 @@ const newWordBtn = document.querySelector('#btn-new-word');
 const screenAddWords = document.querySelector('#screen-add-words');
 const cancelBtn = document.querySelector('#btn-cancel');
 const saveWord = document.querySelector('#btn-save-word');
+const gameScreen = document.querySelector('#game-screen');
 
 const newWordInput = document.querySelector('#new-word');
 
-
 const startScreenClass = startScreen.className;
 const wordScreenClass = screenAddWords.className;
+const gameScreenClass = gameScreen.className;
 
 newGameBtn.addEventListener('click', () => {
   if (startScreenClass.indexOf('hidden') === -1) {
     startScreen.classList.add('hidden');
+    gameScreen.classList.remove('hidden');
   }
 });
 
@@ -36,20 +38,26 @@ cancelBtn.addEventListener('click', () => {
 /* função para inserir novas palavras no array */
 saveWord.addEventListener('click', () => {
   const newWordValue = newWordInput.value;
-  const novasPalavras = newWordValue.split(', ');
-  console.log(novasPalavras);
-  for (let i = 0; i < novasPalavras.length; i += 1) {
-    novasPalavras[i] = novasPalavras[i].toUpperCase();
-    novasPalavras[i] = novasPalavras[i].normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-  }
-  for (let index = 0; index < novasPalavras.length; index += 1) {
-    if (palavras.includes(novasPalavras[index])) {
-      alert('palavra já inserida');
-    } else {
-      palavras.push(novasPalavras[index]);
+  if (newWordValue !== '') {
+    const novasPalavras = newWordValue.split(', ');
+    console.log(novasPalavras);
+    for (let i = 0; i < novasPalavras.length; i += 1) {
+      novasPalavras[i] = novasPalavras[i].toUpperCase();
+      novasPalavras[i] = novasPalavras[i].normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    } /* adicionar popup sobre as letras maiusculas e não usar caracteres com acento e ç */
+    for (let index = 0; index < novasPalavras.length; index += 1) {
+      if (palavras.includes(novasPalavras[index])) {
+        alert(`A palavra ${novasPalavras[index]} já está incluída no jogo`);
+      } else {
+        palavras.push(novasPalavras[index]);
+        screenAddWords.classList.add('hidden');
+        gameScreen.classList.remove('hidden');
+      }
     }
+    console.log(palavras);
+  } else { /* criar popup informando que o campo não pode estar em branco */
+    alert('Favor inserir uma palavra, ou pressione cancelar, para retornar para tela principal');
   }
-  console.log(palavras);
 });
 
 /* função desenhar jogo */
